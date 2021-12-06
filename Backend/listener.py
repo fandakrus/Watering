9f2a490d8cec869d617c282262940929d3706bb0
fandakrus <krus.fratnisek@gmail.com>
2021-12-06 22:07:17 +0100

    Front end controls added and layout setted. Beging with listener.py code restruct

--- Backend/listener.py
+++ Backend/listener.py
@@ -1,78 +1,16 @@
 import socket
 import json
-import mariadb
 from time import sleep
 import logging
 import sys
+from sensors import handle_sensors
 
 # configure logging to given file for better bug finding
 logging.basicConfig(filename="/var/log/python-log/error-log", filemode="w", level=logging.DEBUG,
                     format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def database_connect(name, count):
-    # connects to db and return given connection object
-    try:
-        conn = mariadb.connect(
-            user="root",
-            password="",
-            host="localhost",
-            port=3306,
-            database=name
-        )
-        logging.info(f"Successfully connected to database {name}")
-    # if connection fails
-    except mariadb.Error as e:
-        if count <= 100:
-            logging.critical("Could not connect to DB")
-            sys.exit(1)
-        logging.error(f"Error connecting to MariaDB Platform: {e}")
-        count += 1
-        sleep(5)
-        # dangerous!! can cycle forever if connection is made exit program
-        return database_connect(name, count)
-    # return connection to given database
-    return conn
-
-
-def write_to_database(data):
-    # write data from sensors to database
-    conn = database_connect("watering", 0)
-    curr = conn.cursor()
-    # insert values to database
-    try:
-        curr.execute("INSERT INTO sensors(soil_humidity, water_height, float_sensor) VALUES (?, ?, ?)",
-                     (data["soil_humidity"], data["water_height"], data["float_sensor"]))
-    except mariadb.Error as e:
-        logging.error(f"Could not write in data because of error: {e}")
-    # makes the changes in given database
-    conn.commit()
-    # end the connection to database
-    conn.close()
-
-
-def value_check(value):
-    # check if value is in some reasonable interval
-    bottom_border = 0
-    upper_border = 10000
-    if value > bottom_border and value < upper_border:
-        return True
-    else:
-        return False
-
-
-def is_valid(data):
-    # check validity of data and return it to request data again or not
-    try:
-        if value_check(data["water_height"]) and value_check(data["soil_humidity"]):
-            if data["float_sensor"] == True or data["float_sensor"] == False:
-                return True
-    except KeyError:
-        logging.error("Invalid params received")
-        return False
-
-
-class Meassurement():
+class Listening():
 
     def __init__(self) -> None:
         # port used for this communication is 12345
@@ -84,6 +22,21 @@ class Meassurement():
         # let socket listen and que up max 5 connections   
         self.m_socket.listen(5)
 
+    def handle_data(self):
+        # makes from recived json file data to further use
+        self.results = json.loads(self.rcvData.decode('utf-8'))
+        # find what kind of data were ricieved
+        try:
+            self.type = self.results["type"]
+        except KeyError:
+            return False
+        if self.type == "sensors":
+            handle_sensors(self.results)
+        elif self.type == "web-ui":
+            pass
+        else:
+            return False
+    
     def listen(self):
         # keep listening for the incoming traffic and handle the connections
         logging.info(f"Socket is listening on port {self.port}.")
@@ -92,27 +45,23 @@ class Meassurement():
             self.rcvData = c.recv(1024)
             # activate when message is received
             if self.rcvData is not None:
-                # makes from recived json file data to further use
-                self.results = json.loads(self.rcvData.decode('utf-8'))
-                # there is tested if data are correct and then are writen to the database
-                if is_valid(self.results):
-                    self.results["soil_humidity"] = float(self.results["soil_humidity"])
-                    self.results["water_height"] = float(self.results["water_height"])
-                    write_to_database(self.results)
+                if Listening.handle_data():
                     # c.send('200')
-                else:
                     pass
+                else:
                     # c.send('500')
+                    pass
                 self.rcvData = None
                 self.results = None
                 c.close()
 
+    
 
 def main():
-    # creates new measurement unit
-    meas = Meassurement()
+    # creates new listening unit
+    listening = Listening()
     # let socket listen while other things happen
-    meas.listen()
+    listening.listen()
 
 
 if __name__ == "__main__":
--- Frontend/controls.php
+++ Frontend/controls.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="./main.css" rel="stylesheet">
+    <link href="./style.css" rel="stylesheet">
     <title>Voda</title>
 </head>
 
@@ -25,6 +25,24 @@
             </div>
         </div>
     </nav>
+    <div class="position-relative my-btn-container">
+        <div class="btn-group position-absolut top-0 start-50 translate-middle-x" role="group" aria-label="Basic checkbox toggle button group">
+            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btncheck1">Okruh 1</label>
+            
+            <input type="checkbox" class="btn-check" id="btncheck2" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btncheck2">Okruh 2</label>
+            
+            <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btncheck3">Okruh 3</label>
+            
+            <input type="checkbox" class="btn-check" id="btncheck4" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btncheck4">Okruh 4</label>
+        </div> 
+    </div>
+    <div class="my-sub-btn-container">
+            <button type="button" class="btn btn-outline-secondary my-btn-center ps-3 pe-3">Zalej</button>
+    </div>   
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
