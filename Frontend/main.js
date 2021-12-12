a284bd5a7c679d2e54a9c2eb785b1aaee04aa68d
fandakrus <krus.fratnisek@gmail.com>
2021-12-12 21:00:41 +0100

    Added function to control page - checkbox writes given values to database

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,12 +1,22 @@
+// send checkboxes to php server on given url
 function sendData(data) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "localhost/reqreceiver.php", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-    data
-}));
+    // uses http get reqeuest and sends data threw given url
+    const url = "reqreceiver.php?changed=true&circle1=" + data["circle1"] + "&circle2=" + data["circle2"] + "&circle3=" + data["circle3"] + "&circle4=" + data["circle4"];
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    console.log('sending');
+    /*
+    xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            alert(xhr.responseText);
+        }
+    }*/
+    xhr.send(data);
 }
 
+
+// make a json from checkboxes
 function checkChecker(elements) {
     data = {
         circle1: elements[0].checked,
@@ -15,15 +25,18 @@ function checkChecker(elements) {
         circle4: elements[3].checked,
     };
     console.log(data);
+    return data;
 }
 
+// waits for change in the selection of checkboxes
 document.addEventListener('DOMContentLoaded', function () {
     const elements = document.querySelectorAll('.btn-check');
 
   
     elements.forEach(element => {
         element.addEventListener('click', (e)=>{
-            checkChecker(elements);
+            data = checkChecker(elements);
+            sendData(data);
         });
      });
   });
--- Frontend/reqreceiver.php
+++ Frontend/reqreceiver.php
@@ -15,7 +15,32 @@
         return $conn;
     }
 
-    //open object with database
+    // open object with database connection
     $conn = OpenDbCon();
+
+
+    if(isset($_GET['changed'])) {
+        $circle1=$_GET['circle1'];
+        $circle2=$_GET['circle2'];
+        $circle3=$_GET['circle3'];
+        $circle4=$_GET['circle4'];
+    }
+
+    if($circle1 == "true" or $circle2 == "true" or $circle3 == "true" or $circle4 == "true") {
+        $main_control = "true";
+    } else {
+        $main_control = "false";
+    }
+
+    $sql = "INSERT INTO controls (main_control, circle1, circle2, circle3, circle4) VALUES (". $main_control .", ". $circle1 .", ". $circle2 .", ". $circle3 .", ". $circle4 .")";
     
+    // instert values into database
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created succesfully";
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    // exit connetion
+    $conn->close();
 ?>
