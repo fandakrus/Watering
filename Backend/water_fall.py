5b3ac6dbb79071b2a67e30f01372f6004c8ac4e4
Frantisek Krus <krus.frantisek@gmail.com>
2022-04-20 20:55:44 +0200

    Working with chrome

--- Backend/water_fall.py
+++ Backend/water_fall.py
@@ -1,13 +1,12 @@
-from optparse import Option
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-# import logging
+import logging
 
 
-# logging.basicConfig(filename="/var/log/python-log/rain-read-log", filemode="w", level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename="/var/log/python-log/rain-read-log", filemode="w", level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 def extract_number(data):
     if data == "neměřitelné" or data == '':
@@ -15,7 +14,7 @@ def extract_number(data):
     try:
         return float(data.split()[0].replace(',', '.'))
     except ValueError as e:
-        # logging.error(f"Data in record is not number or anyting expected and error is: {e}")
+        logging.error(f"Data in record is not number or anyting expected and error is: {e}")
         raise OSError
     
     
@@ -29,7 +28,7 @@ class WaterFallData:
         try:
             browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         except FileNotFoundError as e:
-            # logging.error(f"Could not open new browser tab with error: {e}")
+            logging.error(f"Could not open new browser tab with error: {e}")
             raise OSError
         browser.get("https://www.chmi.cz/aktualni-situace/aktualni-stav-pocasi/ceska-republika/stanice/profesionalni-stanice/tabulky/srazky")
         self.html = browser.page_source
@@ -41,7 +40,7 @@ class WaterFallData:
         soup = BeautifulSoup(self.html, features="html.parser")
         table = soup.findAll("table", attrs={"style": "border-top: solid #14387f 1pt; border-bottom: solid #14387f 1pt"})[1]
         if table == []:
-            # logging.info("No table found in recieved html code")
+            logging.info("No table found in recieved html code")
             raise OSError
         datasets = []
         for row in table.find_all("tr")[1:]:
@@ -50,12 +49,12 @@ class WaterFallData:
         try:
             self.record = datasets[5]
         except IndexError as e:
-            # logging.info("Dataset from table is find properly with error: {e}")
+            logging.info("Dataset from table is find properly with error: {e}")
             raise OSError
 
     def get_rain_value(self):
         if self.record[0] != "Plzeň-Mikulka":
-            # logging.error("Wrong dataset read from webpage")
+            logging.error("Wrong dataset read from webpage")
             raise OSError
         self.rain = 0
         for i in range(2, 5):
