851fa16b01eeb1c51c3aa8da7a721e7c1428f376
Frantisek Krus <krus.frantisek@gmail.com>
2022-04-20 15:06:47 +0200

    Micropython code finished - in main used negative values for pins

--- Micropython/workSpace/main.py
+++ Micropython/workSpace/main.py
@@ -1,34 +1,42 @@
 import network
 from time import sleep
-from machine import Pin, ADC
+from machine import Pin, ADC, WDT
 try:
   import usocket as socket 
 except:
   import socket
 import json
 # not defaul lib needs to be upload on ESP
-from hcsr04 import HCSR04
+from hcsr04_lib import HCSR04
+import gc
 
 well_hight = 200
 # analog output from moisture sensor shut be connected to this pin
-moisture_sensor_pin = ADC(Pin(36))   
+moisture_sensor_pin = ADC(Pin(34))   
 moisture_sensor_pin.atten(ADC.ATTN_11DB)
 # connects sensor with trig pin on GPI12 and echo pin on GPIO14
 # timeout set to 20000 for longer distances measurement
 hcsr04_sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=20000)
+gc.enable()
+wdt = WDT(timeout=20000)
 
+
+def value_change(value):
+    if value:
+        return 0
+    return 1
 
 def check_pin(pin, value):
-  if pin.value() != value:
+  if pin.value() == value:
    # print(value)
-   pin.value(value)
+   pin.value(value_change(value))
    
 
 def measure():
     humidity_values = [0] * 5
     # gets 5 values from moisture sensor
-    for value in values:
-        value = moisture_sensor_pin.read()
+    for i in range(len(humidity_values)):
+        humidity_values[i] = moisture_sensor_pin.read()
         sleep(0.5)
     # gets distance from water surface
     distance = hcsr04_sensor.distance_cm()
@@ -38,16 +46,17 @@ def measure():
         "float_sensor": True,
         "water_height": well_hight - distance,
         }
+    print(f"sending data {data}")
     return data
 
 
 class Watering:
   def __init__(self):
-    self.main_valve_pin = Pin(32, Pin.OUT)
-    self.circle1_pin = Pin(33, Pin.OUT)
-    self.circle2_pin = Pin(25, Pin.OUT)
-    self.circle3_pin = Pin(26, Pin.OUT)
-    self.circle4_pin = Pin(27, Pin.OUT)
+    self.main_valve_pin = Pin(32, Pin.OUT, value=1)
+    self.circle1_pin = Pin(33, Pin.OUT, value=1)
+    self.circle2_pin = Pin(25, Pin.OUT, value=1)
+    self.circle3_pin = Pin(26, Pin.OUT, value=1)
+    self.circle4_pin = Pin(27, Pin.OUT, value=1)
     
   def set_pins(self, data):
       # for each pin used check its value and change it based on recieved data
@@ -70,10 +79,11 @@ def do_connect():
         sta_if.connect('ssid', 'passwd')
         while not sta_if.isconnected():
             pass
+    wdt.feed()
     print('network config:', sta_if.ifconfig())
     
 def send(data, require_response=False):
-    # send data to required server
+    # send data to required server`
     
     try:
       server = socket.socket()
@@ -97,21 +107,25 @@ def main():
    do_connect()
    counter = 0
    while True:
-      if counter == 360:
-          # onec in 360 cyrcles measures data and send them to server
+      wdt.feed()
+      if counter == 0:
         data = json.dumps(measure())
         send(data)
+        sleep(2)
+        wdt.feed()
+      # get data for each cyrcle and decide wheter is sould changes values on pins
+      response = send(json.dumps({"type": 1}), require_response=True)
+      try:
+          unjsoned_resp = json.loads(response)
+          print(unjsoned_resp)
+          watering.set_pins(unjsoned_resp)
+      except TypeError as e:
+          print(f"Response not recieved: ({e})")
+      counter += 1
+      if counter == 360:
+        # one in 360 cyrcles measures data and send them to server
         counter = 0
-      else:
-          # get data for each cyrcle and decide wheter is sould changes values on pins
-        response = send(json.dumps({"type": 1}), require_response=True)
-        try:
-            unjsoned_resp = json.loads(response)
-            print(unjsoned_resp)
-            watering.set_pins(unjsoned_resp)
-        except TypeError as e:
-            print(f"Response not recieved: ({e})")
-        counter += 1
+      gc.collect()
       sleep(5)
         
    
